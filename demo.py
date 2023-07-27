2bd50cd9071858af1a217af42d6cedd10b89cf72
ivan.knezovic <ivan.knezovic@kiwi.com>
2023-07-27 15:47:29 +0200

    added demo.py
    added demo.py , subject to changes

---
+++ demo.py
@@ -0,0 +1,32 @@
+import asyncio
+import time
+
+def deliver_messages(city: str):
+    print(f"Message delivered to {city}")
+
+def deliver_to_split(loop):
+    print("sending message to Split")
+    loop.call_later(1, deliver_messages, "SPLIT")
+
+def deliver_to_zadar(loop):
+    print("sending message to Zadar")
+    loop.call_later(3, deliver_messages, "ZADAR")
+    
+def deliver_to_zagreb(loop):
+    print("sending message to Zagreb")
+    loop.call_later(5, deliver_messages, "ZAGREB")
+
+
+def main():
+    try:
+        loop = asyncio.new_event_loop()
+        deliver_to_zagreb(loop)
+        deliver_to_zadar(loop)
+        deliver_to_split(loop)
+
+        loop.run_until_complete(asyncio.sleep(10))
+    finally:
+        loop.close()
+
+if __name__ == '__main__':
+    main()
